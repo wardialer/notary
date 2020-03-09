b1ad11d1ba82a6b8af5adb95d857f65ffaf1fab1
Giuseppe Raveduto <giuseppe.raveduto@eng.it>
2020-03-09 23:44:41 +0100

    moving database connection from app.js to server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,14 @@
 const cron = require('cron');
+const mongoose = require('mongoose');
 const app = require('./app');
 const bitcoinLib = require('./libs/bitcoin');
+
+const databaseHost = process.env.DB_HOST || 'localhost';
+const databaseName = process.env.DB_NAME || 'bitcoin-notary';
+
+mongoose.connect(`mongodb://${databaseHost}/${databaseName}`, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port);
